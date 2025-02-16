80c55d605bea629801fe14a6a0353e79392811bc
Drew Cook <dc333@usf.edu>
2025-02-16 16:37:45 -0500

    Fixed formatting of answers for task 1 and 2. Fixed second test data for task 2.

--- cc_7.js
+++ cc_7.js
@@ -1,7 +1,7 @@
 //Task 1: function declaration. Customer invoice calculation scenario.
 function calculateInvoice(subtotal, taxRate, discount) { //declare function
     total = (subtotal + (subtotal * taxRate)) - discount; //use formula to calculate the total as a variable
-    return `Total Invoice ${total}`; //return the calculated variable
+    return `Total Invoice $${total.toFixed(2)}`; //return the calculated variable
 }
 
 console.log(calculateInvoice(100, 0.08, 5)); //test data
@@ -10,11 +10,11 @@ console.log(calculateInvoice(500, 0.1, 20));
 //Task 2: function expression. Employee hourly wage calculation scenario.
 const calculateHourlyWage = function(salary, hoursPerWeek) { //function expression
     wage = salary / (hoursPerWeek * 52); //use formula to calculate hourly wage based on hours worked and yearly wage
-    return `Hourly Wage: ${wage}`; //return a string that uses template literals to insert the calculated wage
+    return `Hourly Wage: $${wage.toFixed(2)}`; //return a string that uses template literals to insert the calculated wage
 }
 
 console.log(calculateHourlyWage(52000, 40)); //test data
-console.log(calculateHourlyWage(52000, 40));
+console.log(calculateHourlyWage(75000, 35));
 
 //Task 3: arrow function. Customer loyalty discount scenario.
 const calculateLoyaltyDiscount = (amount, years) => { //use arrow to create function
