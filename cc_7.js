e8ea47176cfa08406b9642dea09c09185ad91356
Drew Cook <dc333@usf.edu>
2025-02-16 16:14:37 -0500

    Task 2 - Employee Hourly Wage Calculation.

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,12 @@ function calculateInvoice(subtotal, taxRate, discount) { //declare function
 
 console.log(calculateInvoice(100, 0.08, 5)); //test data
 console.log(calculateInvoice(500, 0.1, 20));
+
+//Task 2: function expression. Employee hourly wage calculation scenario.
+const calculateHourlyWage = function(salary, hoursPerWeek) { //function expression
+    wage = salary / (hoursPerWeek * 52); //use formula to calculate hourly wage based on hours worked and yearly wage
+    return `Hourly Wage: ${wage}`; //return a string that uses template literals to insert the calculated wage
+}
+
+console.log(calculateHourlyWage(52000, 40)); //test data
+console.log(calculateHourlyWage(52000, 40));
